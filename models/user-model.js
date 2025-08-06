079b49356c00e5b897fc840fe8e0fce19ada2a65
Your Name <kartikmishra686@gmail.com>
2025-08-07 01:45:06 +0530

    Just Checking

---
+++ models/user-model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/MONGODB_PROJECT");
+
+const userSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cart",
+        default: []
+    },
+    isAdmin: Boolean,
+    orders: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+        default: []
+    },
+    contact: Number,
+    picture: String
+});
